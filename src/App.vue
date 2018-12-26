bc3c0a00c8a21a480bf7ce6afcd0f1df426cdd84
mokunshao <1627510957@qq.com>
2018-12-26 23:24:47 +0800

    修改样式

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <div id="cover"></div>
     <Header/>
     <Todo/>
     <Footer/>
@@ -23,21 +22,4 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  #cover {
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    background: #999;
-    opacity: 0.3;
-    z-index: -1;
-  }
-}
 </style>
